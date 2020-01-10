1892bc13579a32cc097d129f2c4f5e423f3e1169
Edwin <edwin@wonderbly.com>
2020-01-10 10:14:29 +0000

    feat: Add desktop option for heading sizes

--- src/styles/typography.styled.js
+++ src/styles/typography.styled.js
@@ -16,43 +16,49 @@ const lineHeight = (value, useOldCalc = false) => css`
   line-height: ${useOldCalc ? ((value + 8) / value) : baseRem(value * 1.618)};
 `;
 
-const fontSize = (value, useOldLineHeightCalc = false) => css`
+const fontSize = (value, { desktopValue, useOldLineHeightCalc = false } = {}) => css`
   font-size: ${baseRem(value)};
   ${lineHeight(value, useOldLineHeightCalc)}
+  ${desktopValue && desktopValue !== value && 
+    `@media screen (min-width: 778px) {
+      font-size: ${baseRem(desktopValue)};
+      ${lineHeight(desktopValue, useOldLineHeightCalc)}
+    }`
+  }
 `;
 
 const tera = css`
-  ${fontSize(30, true)}
+  ${fontSize(30, { desktopValue: 36, useOldLineHeightCalc: true })}
   font-weight: 600;
 `;
 
 const giga = css`
-  ${fontSize(24, true)}
+  ${fontSize(24, { desktopValue: 30, useOldLineHeightCalc: true })}
   font-weight: 500;
 `;
 
 const mega = css`
-  ${fontSize(20, true)}
+  ${fontSize(20, { desktopValue: 24, useOldLineHeightCalc: true })}
   font-weight: 400;
 `;
 
 const kilo = css`
-  ${fontSize(18, true)}
+  ${fontSize(18, { desktopValue: 20, useOldLineHeightCalc: true })}
   font-weight: 400;
 `;
 
 const base = css`
-  ${fontSize(16, true)}
+  ${fontSize(16, { useOldLineHeightCalc: true })}
   font-weight: 400;
 `;
 
 const milli = css`
-  ${fontSize(16, true)}
+  ${fontSize(16, { useOldLineHeightCalc: true })}
   font-weight: 300;
 `;
 
 const micro = css`
-  ${fontSize(14, true)}
+  ${fontSize(14, { useOldLineHeightCalc: true })}
   font-weight: 300;
 `;
 
